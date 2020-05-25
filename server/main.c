f788d3acf59dc3746a41fe8ccd25253f98ef9ece
aburdulescu <andrei.burdulescu@pm.me>
2020-05-25 20:52:14 +0300

    add shutdown mechanism for listeners

--- server/main.c
+++ server/main.c
@@ -1,6 +1,8 @@
 #include <errno.h>
+#include <mqueue.h>
 #include <netdb.h>
 #include <pthread.h>
+#include <signal.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -32,7 +34,6 @@ static int onRead(int fd) {
   if (rc == 0) {
     return 1;
   }
-  /* printf("%s: %d: %d %s\n", __FUNCTION__, fd, rc, (char*)buf); */
   rc = send(fd, buf, rc, 0);
   if (rc == EAGAIN || rc == EWOULDBLOCK) {
     printf("%s: writing to %d would block\n", __FUNCTION__, fd);
@@ -46,6 +47,7 @@ static int onRead(int fd) {
 }
 
 static void* onAccept(void* args) {
+  int mq = *((int*)args);
   NetListener l;
   int rc = net_listen(&l, "tcp", NULL, "55443", 100);
   if (rc < 0) {
@@ -65,6 +67,13 @@ static void* onAccept(void* args) {
     perror("epoll_ctl: listen_sock");
     return NULL;
   }
+  ev.events = EPOLLIN;
+  ev.data.fd = mq;
+  rc = epoll_ctl(epollfd, EPOLL_CTL_ADD, mq, &ev);
+  if (rc < 0) {
+    perror("epoll_ctl: mq");
+    return NULL;
+  }
   struct epoll_event events[MAX_EVENTS];
   for (;;) {
     int nfds = epoll_wait(epollfd, events, MAX_EVENTS, -1);
@@ -73,7 +82,22 @@ static void* onAccept(void* args) {
       goto error;
     }
     for (int n = 0; n < nfds; ++n) {
-      if (events[n].data.fd != l.fd) {
+      if (events[n].data.fd == l.fd) {
+        NetConn c;
+        rc = net_accept(&l, &c);
+        if (rc == -1) {
+          goto error;
+        }
+        ev.events = EPOLLIN;
+        ev.data.fd = c.fd;
+        rc = epoll_ctl(epollfd, EPOLL_CTL_ADD, c.fd, &ev);
+        if (rc < 0) {
+          perror("epoll_ctl add conn socket");
+          goto error;
+        }
+      } else if (events[n].data.fd == mq) {
+        goto error;
+      } else {
         rc = onRead(events[n].data.fd);
         if (rc == 0) {
           continue;
@@ -84,29 +108,53 @@ static void* onAccept(void* args) {
           goto error;
         }
         close(events[n].data.fd);
-        /* printf("%s: %d closed\n", __FUNCTION__, events[n].data.fd); */
-        continue;
-      }
-      NetConn c;
-      rc = net_accept(&l, &c);
-      if (rc == -1) {
-        goto error;
-      }
-      ev.events = EPOLLIN;
-      ev.data.fd = c.fd;
-      rc = epoll_ctl(epollfd, EPOLL_CTL_ADD, c.fd, &ev);
-      if (rc < 0) {
-        perror("epoll_ctl add conn socket");
-        goto error;
       }
     }
   }
 error:
   close(l.fd);
+  close(epollfd);
+  // TODO: close registered conn sockets
   return NULL;
 }
 
-// TODO: handle server shutdown
+const size_t MQ_MAX_NAME = 16;
+
+int createMq(int i, char* mqName) {
+  const char mqNamePrefix[] = "/epolls_mq_";
+  const size_t mqNamePrefixSize = strlen(mqNamePrefix);
+  memset(mqName, 0, MQ_MAX_NAME);
+  strncpy(mqName, mqNamePrefix, mqNamePrefixSize);
+  struct mq_attr attr;
+  attr.mq_flags = 0;
+  attr.mq_maxmsg = 10;
+  attr.mq_msgsize = 256;
+  attr.mq_curmsgs = 0;
+  char istr[2];
+  memset(istr, 0, 2);
+  sprintf(istr, "%d", i);
+  strncpy(mqName + mqNamePrefixSize, istr, 1);
+  int rc = mq_open(mqName, O_RDWR | O_CREAT | O_NONBLOCK, 0660, &attr);
+  if (rc < 0) {
+    perror("mq_open");
+    return -1;
+  }
+  return rc;
+}
+
+static int gExit = 0;
+
+void onSignal(int s) {
+  gExit = 1;
+}
+
+typedef struct {
+  int* args;
+  pthread_t id;
+  int mq;
+  char mqName[MQ_MAX_NAME];
+} ListenerInfo;
+
 // TODO: use multiple threads for request
 // handling(https://idea.popcount.org/2017-02-20-epoll-is-fundamentally-broken-12/)
 int main(int argc, char* argv[]) {
@@ -114,20 +162,46 @@ int main(int argc, char* argv[]) {
     fprintf(stderr, "error: need listeners count\n");
     return 1;
   }
-  int listeners = atoi(argv[1]);
-  pthread_t tids[listeners];
+  signal(SIGINT, onSignal);
+  signal(SIGSEGV, onSignal);
+  signal(SIGABRT, onSignal);
+  const int listenersSize = atoi(argv[1]);
+  ListenerInfo listeners[listenersSize];
+  memset(listeners, 0, sizeof(listeners));
   int rc;
-  for (int i = 0; i < listeners; ++i) {
-    rc = pthread_create(&tids[i], NULL, &onAccept, NULL);
+  for (int i = 0; i < listenersSize; ++i) {
+    int mq = createMq(i, listeners[i].mqName);
+    if (mq < 0) {
+      continue;
+    }
+    listeners[i].mq = mq;
+    listeners[i].args = (int*)malloc(sizeof(int));
+    *(listeners[i].args) = mq;
+    rc = pthread_create(&listeners[i].id, NULL, &onAccept, listeners[i].args);
     if (rc != 0) {
       perror("phread_create");
+      continue;
     }
   }
-  for (int i = 0; i < listeners; ++i) {
-    rc = pthread_join(tids[i], NULL);
+  while (!gExit) {
+  }
+  const char buf[] = "dummy";
+  for (int i = 0; i < listenersSize; ++i) {
+    rc = mq_send(listeners[i].mq, buf, sizeof(buf), 0);
+    if (rc < 0) {
+      perror("mq_send");
+      continue;
+    }
+    rc = pthread_join(listeners[i].id, NULL);
     if (rc != 0) {
       perror("phread_join");
     }
+    close(listeners[i].mq);
+    rc = mq_unlink(listeners[i].mqName);
+    if (rc != 0) {
+      perror("mq_unlink");
+    }
+    free(listeners[i].args);
   }
   return 0;
 }
